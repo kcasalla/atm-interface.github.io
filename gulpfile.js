959806525cd7bee530097583153c73d27dfc4b82
Kaye <kaye@autodeal.com.ph>
2022-04-20 00:15:47 +0800

    added welcome screen and initial screen.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browsersync = require('browser-sync').create();
 
 function buildStyles() {
     return gulp.src('app/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(browsersync.stream());
 }
 
 function watch() {
+    browsersync.init({
+        server: {
+            baseDir: ['.'],
+            index: ['index.html']
+        },
+    });
+
     gulp.watch('app/scss/**/*.scss', buildStyles);
+    gulp.watch('**/*.html').on('change', browsersync.reload);
+    // gulp.watch('app/js/**/*.js', browsersync); -- for JS
 }
 
 exports.buildStyles = buildStyles;
